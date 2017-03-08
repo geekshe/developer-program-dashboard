a58eb23b1ca3ef455ddc1e80a10c178e37171414
geekshe <laurel@geekshe.com>
2017-03-08 02:01:32 +0000

    Small cosmetic changes to support demo

--- server.py
+++ server.py
@@ -156,7 +156,7 @@ if __name__ == "__main__":
     connect_to_db(app)
 
     # Use the DebugToolbar
-    DebugToolbarExtension(app)
+    # DebugToolbarExtension(app)
 
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
